b53902d93f1da8fa461c71f0fbc0e78b28b67f83
Rekam <71652287+Yidti@users.noreply.github.com>
2024-05-08 02:56:34 +0800

    pie chart

--- data_analysis.py
+++ data_analysis.py
@@ -10,6 +10,7 @@ import sqlite3
 import pymongo
 from wordcloud import WordCloud
 import platform
+import numpy as np
 
 def set_font():
     """
@@ -86,7 +87,7 @@ class DataAnalysis(metaclass=SingletonMeta):
             df = pd.DataFrame()  # 雖然不是必需,但保留這行也沒有問題
         return df
 
-    def plot_countplot(self, df, column_name, ax=None, figsize=(10, 6), vertical=True, rotation=0):
+    def plot_count(self, df, column_name, ax=None, figsize=(10, 6), vertical=True, rotation=0):
 
         if df.empty:
             print("DataFrame 为空,无法绘制计数柱状图。")
@@ -145,5 +146,140 @@ class DataAnalysis(metaclass=SingletonMeta):
         ax.set_ylim(top= y_limit_2 + diff)
         ax.set_ylim(bottom= y_limit_1 - diff)
 
-        
         plt.tight_layout()
+
+    
+    def plot_pie(self, df, column_name, ax=None, figsize=(10, 6), startangle=45, x_move=1.4, y_move=1.2, y_add=0.2):
+        if df.empty:
+            print("DataFrame 為空,無法繪製圖形。")
+            return
+    
+        if column_name not in df.columns:
+            print(f"列名 '{column_name}' 不存在於 DataFrame 中。")
+            return
+    
+        if ax is None:
+            fig, ax = plt.subplots(figsize=figsize)
+    
+        value_counts = df[column_name].value_counts()
+        labels = value_counts.index
+        sizes = value_counts.values
+        colors = sns.color_palette("husl", len(labels))
+        total = sum(sizes)  # 計算總數
+        results = []
+        for label, size in zip(labels, sizes):
+            percentage = (size / total) * 100  # 計算百分比
+            result = f"{label} ({percentage:.2f}%)"
+            results.append(result)
+
+        wedges, texts = ax.pie(sizes, wedgeprops=dict(width=0.5), startangle=startangle)
+        bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+        kw = dict(arrowprops=dict(arrowstyle="-"), bbox=bbox_props, zorder=0, va="center")
+
+        prev_ang = 0
+        min_ang = 8  # 調整這個值以控制最小距離
+        for i, p in enumerate(wedges):
+            ang_origin = (p.theta2 - p.theta1)/2. + p.theta1
+            y = np.sin(np.deg2rad(ang_origin))
+            x = np.cos(np.deg2rad(ang_origin))
+            horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+            connectionstyle = "angle,angleA=0,angleB={}".format(ang_origin)
+            kw["arrowprops"].update({"connectionstyle": connectionstyle})
+            
+            # 檢查新的 annotate 位置是否太靠近之前的 annotate 位置
+            too_close = False
+            diff_ang = np.abs(prev_ang-ang_origin)
+            # print("diff_ang",diff_ang)
+            if diff_ang < min_ang:
+                too_close = True
+                
+            if too_close:
+            # 如果太靠近,調整新的 annotate 位置
+                # print("Change")
+                ang = prev_ang + 8
+                yy = np.sin(np.deg2rad(ang))
+                xx = np.cos(np.deg2rad(ang))
+                horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(xx))]
+                connectionstyle = "angle,angleA=0,angleB={}".format(-ang)
+                # 位移
+                y_move += y_add
+                xytext = (x_move*np.sign(x), y_move*y)
+            else:
+                xytext = (x_move*np.sign(x), y_move*y)
+            
+            prev_ang = ang_origin
+
+            ax.annotate(results[i], xy=(x, y), xytext=xytext,
+                        horizontalalignment=horizontalalignment, **kw)
+
+
+        # for i, p in enumerate(wedges):
+        #     ang_origin = (p.theta2 - p.theta1)/2. + p.theta1
+        #     y = np.sin(np.deg2rad(ang_origin))
+        #     x = np.cos(np.deg2rad(ang_origin))
+        #     print("ang",ang_origin, x, y)
+        #     horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        
+        #     connectionstyle = f"angle,angleA=0,angleB={ang_origin}"
+        #     kw["arrowprops"].update({"connectionstyle": connectionstyle})
+        
+        #     # 檢查新的 annotate 位置是否太靠近之前的 annotate 位置
+        #     too_close = False
+            
+        #     # distance = np.sqrt((x - prev_x)**2 + (y - prev_y)**2)
+        #     diff_ang = np.abs(prev_ang-ang_origin)
+        #     print("diff_ang",diff_ang)
+        #     if diff_ang < min_ang:
+        #         too_close = True
+
+            
+        #     if too_close:
+        #         # 如果太靠近,調整新的 annotate 位置
+        #         print("Change")
+        #         ang = prev_ang + 8
+        #         prev_ang = ang
+
+        #         # y = np.sin(np.deg2rad(ang))
+        #         # x = np.cos(np.deg2rad(ang))
+        #         horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        #         connectionstyle = f"angle,angleA=0,angleB={ang}"
+        #         kw["arrowprops"].update({"connectionstyle": connectionstyle})
+                
+        #         text_x = 1.3*np.sign(x)
+        #         text_y = 1.4*y
+        #         # text_x, text_y = x * 1.3, y * 1.2
+
+        #         print('Change',f'{results[i]}',ang)
+
+        #     else:
+        #         prev_ang = ang_origin
+        #         # text_x, text_y = x * 1.3, y * 1.2
+
+        #         text_x = 1.3*np.sign(x)
+        #         text_y =1.2*y
+
+            
+            
+        #     if i in range(0,8):
+        #         # print(results[i], x, y, text_x, text_y)
+        #         ann = ax.annotate(results[i], xy=(x, y), xytext=(text_x, text_y),
+        #                           horizontalalignment=horizontalalignment, **kw)
+        #         # 記錄新的 annotate 位置
+        #         prev_x= ann.xy[0]
+        #         prev_y= ann.xy[1]
+
+            
+
+
+        
+        
+        ax.axis('equal')  # 確保圓環圖是圓形的
+        ax.set_title(f"{column_name}")
+    
+        # 移除邊框
+        ax.set_frame_on(False)
+        plt.tight_layout()
+
+        
+
+    
